27fefe7a6726a4d3f03fefd101712de5b9688d53
Nathalie <71539632+Ayashinat@users.noreply.github.com>
2026-02-17 20:22:00 +0100

    app
    Removed title and introductory text, added input fields for name and message.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,4 @@
 import streamlit as st
-
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+prenom = st.text_input('Quel est votre prénom ?')
+message = st.text_input(' Quel est votre message ?')
+st.write(message," ", prenom)
