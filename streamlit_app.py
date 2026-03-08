f86065794dd4f9451e29041ee8162f4799f4f791
Ayashinat <ayashinat@hotmail.com>
2026-03-08 12:07:57 +0100

    upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,47 +2,35 @@ import streamlit as st
 from PIL import ExifTags, Image
 from streamlit_folium import st_folium
 
+import folium
 
 st.set_page_config(page_title="OIC 4.2 - EXIF GPS", layout="wide")
 st.title("OIC 4.2 - Edition EXIF + GPS + Cartes")
 
-charger_photo = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+charger_photo = st.file_uploader("Choisir une image", type=["jpg", "jpeg", "png"])
 
-if charger_photo is not None:
-	image = Image.open(charger_photo)
-	exif_data = image._getexif()
-	
-	if exif_data is not None:
-		st.subheader("EXIF Data")
-		for tag_id, value in exif_data.items():
-			tag = ExifTags.TAGS.get(tag_id, tag_id)
-			st.write(f"{tag}: {value}")
-		
-		gps_info = exif_data.get(34853)  # GPSInfo tag
-		if gps_info is not None:
-			st.subheader("GPS Information")
-			gps_data = {}
-			for key in gps_info.keys():
-				gps_tag = ExifTags.GPSTAGS.get(key, key)
-				gps_data[gps_tag] = gps_info[key]
-				st.write(f"{gps_tag}: {gps_info[key]}")
-			
-			# Extract latitude and longitude
-			lat = gps_data.get('GPSLatitude')
-			lat_ref = gps_data.get('GPSLatitudeRef')
-			lon = gps_data.get('GPSLongitude')
-			lon_ref = gps_data.get('GPSLongitudeRef')
-			
-			if lat and lat_ref and lon and lon_ref:
-				lat_decimal = (lat[0][0] / lat[0][1]) + (lat[1][0] / lat[1][1]) / 60 + (lat[2][0] / lat[2][1]) / 3600
-				if lat_ref == 'S':
-					lat_decimal = -lat_decimal
-				
-				lon_decimal = (lon[0][0] / lon[0][1]) + (lon[1][0] / lon[1][1]) / 60 + (lon[2][0] / lon[2][1]) / 3600
-				if lon_ref == 'W':
-					lon_decimal = -lon_decimal
-				
-				st.subheader("Location on Map")
-				st_folium({"latitude": lat_decimal, "longitude": lon_decimal}, width=700, height=500)
-	else:
-		st.warning("No EXIF data found in the image.")
+if charger_photo:
+    image = Image.open(charger_photo)
+    st.image(image, caption="Image chargée", use_container_width=True)
+
+    exif = image.getexif()
+
+    if exif:
+        exif_lisible = {}
+        for tag_id, valeur in exif.items():
+            nom_tag = ExifTags.TAGS.get(tag_id, tag_id)
+            exif_lisible[nom_tag] = valeur
+
+        st.subheader("Métadonnées EXIF")
+        st.write(exif_lisible)
+
+        gps = exif_lisible.get("GPSInfo")
+
+        if gps is not None:
+            st.subheader("Balise GPS brute")
+            st.write(gps)
+            st.write("Type :", type(gps))
+        else:
+            st.info("Aucune donnée GPS trouvée.")
+    else:
+        st.info("Aucune métadonnée EXIF détectée.")
